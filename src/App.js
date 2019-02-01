8ca72521afe3d81aded3c846b82c01643e19eda2
Mike DeGan <mikedegan@gmail.com>
2019-01-31 21:09:24 -0500

    just a couple more buttons

--- src/App.js
+++ src/App.js
@@ -27,16 +27,20 @@ class App extends Component {
           </svg>
         </div>
         <div id="break-label">Break Length</div>
-        <button id="break-decrement" />
+        <div className="arrowholder">
+          <SVGUpArrow id="break-increment" onClick={this.handleDownArrow} />
+          <SVGDownArrow id="break-decrement" onClick={this.handleDownArrow} />
+        </div>
+
         <div id="break-length" />
-        <button id="break-increment" />
+
         <div id="session-label">Session Length</div>
         <div className="arrowholder">
           <SVGUpArrow id="session-increment" onClick={this.handleDownArrow} />
           <SVGDownArrow id="session-decrement" onClick={this.handleDownArrow} />
         </div>
         <div id="session-length" />
-        <button id="session-increment" />
+
         <div id="timer-label">Session</div>
         <div id="time-left">mm:ss</div>
         <div className="btn-group">
