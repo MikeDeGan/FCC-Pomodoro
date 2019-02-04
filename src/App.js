68d8a79f4d72a555f9751637c985947e28255a9a
Mike DeGan <mikedegan@gmail.com>
2019-02-03 22:13:03 -0500

    passing all FCC tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
-import SVGDownArrow from './SVGDownArrow';
-import SVGUpArrow from './SVGUpArrow';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sessionLength: 20,
+      sessionLength: 25,
       breakLength: 5,
-      secRemaining: 1200,
-      circleTime: 10,
-      minDisplay: '20',
+      secRemaining: 1500,
+      circleTime: 0,
+      minDisplay: '25',
       secDisplay: '00',
-      timerState: 'paused'
+      timerState: 'paused',
+      timerType: 'session',
+      alarmState: 'paused',
+      startButton: 'Start'
     };
     this.handleIncreaseBreak = this.handleIncreaseBreak.bind(this);
     this.handleDecreaseBreak = this.handleDecreaseBreak.bind(this);
@@ -66,9 +67,9 @@ class App extends Component {
       return;
     }
     sLength =
-      sLength < 10 ? (sLength = '0' + (sLength - 1).toString()) : sLength - 1;
+      sLength <= 10 ? (sLength = '0' + (sLength - 1).toString()) : sLength - 1;
     this.setState({
-      sessionLength: sLength,
+      sessionLength: sLength * 1,
       minDisplay: sLength.toString(),
       secDisplay: '00',
       secRemaining: sLength * 60
@@ -76,16 +77,61 @@ class App extends Component {
     });
   }
 
+  switchTimerType() {
+    let newTimerType;
+    let newSecRemaining;
+    // let newMinDisplay;
+    let newCircleTime;
+    const { breakLength, sessionLength, timerType } = this.state;
+    const elem = document.getElementById('beep');
+    elem.play();
+    this.setState({ alarmState: 'running' });
+    setTimeout(() => {
+      this.setState({ alarmState: 'paused' });
+    }, 1000);
+
+    if (timerType === 'session') {
+      newTimerType = 'break';
+      newSecRemaining = breakLength * 60 + 1;
+      // newMinDisplay = '00';
+      newCircleTime = 0;
+    } else {
+      newTimerType = 'session';
+      newSecRemaining = sessionLength * 60 + 1;
+      // newMinDisplay = '00';
+      newCircleTime = 0;
+    }
+
+    this.setState({
+      timerType: newTimerType,
+      secRemaining: newSecRemaining,
+      secDisplay: '00',
+      circleTime: newCircleTime
+    });
+  }
+
   handleStart() {
-    this.setState({ timerState: 'running' });
+    if (this.state.timerState === 'running') {
+      this.setState({ timerState: 'paused', startButton: 'Start' });
+      return;
+    }
+    this.setState({ timerState: 'running', startButton: 'Pause' });
     const timerID = setInterval(() => {
       if (this.state.timerState === 'paused') {
         clearInterval(timerID);
         return;
       }
       const secs = this.state.secRemaining - 1;
-      const cTime = (100.53088 / 1200) * (1200 - secs);
-      console.log(cTime);
+      if (secs < 1) {
+        this.switchTimerType();
+        return;
+      }
+      const startTime =
+        this.state.timerType === 'session'
+          ? this.state.sessionLength * 60
+          : this.state.breakLength * 60;
+      const cTime = (100.53088 / startTime) * (startTime - secs);
+
       let newMinDisplay =
         Math.floor(secs / 60) < 10
           ? '0' + Math.floor(secs / 60).toString()
@@ -103,21 +149,44 @@ class App extends Component {
   }
 
   handleReset() {
-    this.setState({ timerState: 'paused' });
+    const elem = document.getElementById('beep');
+    elem.pause();
+    elem.load();
+
+    this.setState({
+      sessionLength: 25,
+      breakLength: 5,
+      secRemaining: 1500,
+      circleTime: 0,
+      minDisplay: '25',
+      secDisplay: '00',
+      timerState: 'paused',
+      timerType: 'session',
+      alarmState: 'paused',
+      startButton: 'Start'
+    });
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">FCC Pomodoro Project</header>
-        <div id="timer-label">Session</div>
+        <div id="timer-label">{this.state.timerType}</div>
         <div className="pieholder">
-          <svg viewBox="0 0 64 64" className="pie">
+          <svg
+            viewBox="0 0 64 64"
+            className="pie"
+            style={{
+              animation: 'flashShadow 0.1s linear infinite alternate',
+              animationPlayState: `${this.state.alarmState}`
+            }}
+          >
             <circle
               r="25%"
               cx="50%"
               cy="50%"
               style={{
-                strokeDasharray: `${this.state.circleTime} 100`
+                strokeDasharray: `${this.state.circleTime} 100.53088`
+
                 // animation: `runClock ${this.state.circleTime}s linear infinite`,
                 // animationPlayState: this.state.timerState
               }}
@@ -131,12 +200,14 @@ class App extends Component {
         <div id="adjustments">
           <div id="break-label">Break Length</div>
           <div className="arrowholder">
-            <SVGUpArrow
+            <i
+              className="fas fa-angle-up"
               id="break-increment"
               onClick={this.handleIncreaseBreak}
             />
             <div id="break-length">{this.state.breakLength}</div>
-            <SVGDownArrow
+            <i
+              className="fas fa-angle-down"
               id="break-decrement"
               onClick={this.handleDecreaseBreak}
             />
@@ -144,12 +215,15 @@ class App extends Component {
 
           <div id="session-label">Session Length</div>
           <div className="arrowholder">
-            <SVGUpArrow
+            <i
+              className="fas fa-angle-up"
               id="session-increment"
               onClick={this.handleIncreaseSession}
             />
+
             <div id="session-length">{this.state.sessionLength}</div>
-            <SVGDownArrow
+            <i
+              className="fas fa-angle-down"
               id="session-decrement"
               onClick={this.handleDecreaseSession}
             />
@@ -158,17 +232,23 @@ class App extends Component {
 
         <div className="btn-group">
           <button
-            id="start-stop"
+            id="start_stop"
             className="btn-info"
             onClick={this.handleStart}
           >
-            Start
+            {this.state.startButton}
           </button>
           <button id="reset" className="btn-info" onClick={this.handleReset}>
             Reset
           </button>
         </div>
-        <audio src="" id="beep" />
+        <audio id="beep">
+          <source
+            src="./sounds/service-bell_daniel_simion.mp3"
+            type="audio/mp3"
+          />
+          Your browser does not support the audio element.
+        </audio>
       </div>
     );
   }
