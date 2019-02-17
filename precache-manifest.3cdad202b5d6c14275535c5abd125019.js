f40e93a207e249dc62526ad9b15d7728c90f2261
Mike DeGan <mikedegan@gmail.com>
2019-02-17 13:18:01 -0500

    Updates

---
+++ precache-manifest.3cdad202b5d6c14275535c5abd125019.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fcdd12bfa0bc70798f7",
+    "url": "/FCC-Pomodoro/static/js/runtime~main.4fcdd12b.js"
+  },
+  {
+    "revision": "a6fb27e37329d0408f00",
+    "url": "/FCC-Pomodoro/static/js/main.a6fb27e3.chunk.js"
+  },
+  {
+    "revision": "d216b6c89879fa8b44c4",
+    "url": "/FCC-Pomodoro/static/js/1.d216b6c8.chunk.js"
+  },
+  {
+    "revision": "a6fb27e37329d0408f00",
+    "url": "/FCC-Pomodoro/static/css/main.dcf1d173.chunk.css"
+  },
+  {
+    "revision": "e2cd93e8dfee8280a1a3deb4a313a9e4",
+    "url": "/FCC-Pomodoro/index.html"
+  }
+];
